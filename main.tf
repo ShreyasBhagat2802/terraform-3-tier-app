0a138168aa34ac9849deafed43300a9ccbd9de60
shreyas <shreyasbhagat2802@gmail.com>
2024-12-05 07:55:59 +0000

    main.tf changed

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+#provider "aws" {
+#  region = var.region
+#}
 
 module "vpc" {
   source                = "./VPC"
